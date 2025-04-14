96bb44886a9619d6cb1dd767e87d07cf6419523b
Rom1xa <abakumoff.roman2018@yandex.ru>
2025-04-14 15:57:28 +0300

    reorganize structure, add src/, update .gitignore

--- src/MiroBoard.py
+++ src/MiroBoard.py
@@ -9,35 +9,41 @@ class MiroBoard:
         self.headers = {
             "Authorization": f"Bearer {self.API_TOKEN}",
             "accept": "application/json",
-            "content-type": "application/json"
+            "content-type": "application/json",
         }
         self.url = f"https://api.miro.com/v2/boards/{self.board_id}/"
 
-    def add_sticker(self, content, color='light_yellow', x=0, y=0):
-        data = {'data': {'content': content},
-                'position': {'x': x, 'y': y},
-                'style': {'fillColor': color}, }
+    def add_sticker(self, content, color="light_yellow", x=0, y=0):
+        data = {
+            "data": {"content": content},
+            "position": {"x": x, "y": y},
+            "style": {"fillColor": color},
+        }
 
-        need_url = self.url + 'sticky_notes'
+        need_url = self.url + "sticky_notes"
         response = requests.post(need_url, headers=self.headers, json=data)
 
         return response.status_code
 
     def add_image(self, url, x=0, y=0):
-        data = {'data': {'url': url},
-                'position': {'x': x, 'y': y}, }
+        data = {
+            "data": {"url": url},
+            "position": {"x": x, "y": y},
+        }
 
-        need_url = self.url + 'images'
+        need_url = self.url + "images"
         response = requests.post(need_url, headers=self.headers, json=data)
 
         return response.status_code
 
     def add_text(self, content, fontSize=14, x=0, y=0):
-        data = {'data': {'content': content},
-                'position': {'x': x, 'y': y},
-                'style': {'fontSize': fontSize}, }
+        data = {
+            "data": {"content": content},
+            "position": {"x": x, "y": y},
+            "style": {"fontSize": fontSize},
+        }
 
-        need_url = self.url + 'texts'
+        need_url = self.url + "texts"
         response = requests.post(need_url, headers=self.headers, json=data)
 
         return response.status_code
