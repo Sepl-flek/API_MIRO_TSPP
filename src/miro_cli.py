ae498287a242889329ec839568c9c3a5848f0a6a
Rom1xa <abakumoff.roman2018@yandex.ru>
2025-05-14 20:25:07 +0300

    update miro_cli.py, finished build_parser func

--- src/miro_cli.py
+++ src/miro_cli.py
@@ -51,6 +51,54 @@ def build_parser() -> argparse.ArgumentParser:
         description="CLI for miro",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
+    p.add_argument("-t", "--token", help="API token miro ( MIRO_API_TOKEN )")
+    p.add_argument("-b", "--board", help="board id ( MIRO_BOARD_ID )")
+
+    sub = p.add_subparsers(dest="command", required=True)
+
+    # sticker
+    s = sub.add_parser("add_sticker", help="add sticker")
+    s.add_argument("text")
+    s.add_argument("--color", default="light_yellow")
+    s.add_argument("--shape", default="square")
+    s.add_argument("--width", type=int, default=199)
+    s.add_argument("--x", type=int, default=0)
+    s.add_argument("--y", type=int, default=0)
+    s.set_defaults(func=cmd_add_sticker)
+
+    # text
+    s = sub.add_parser("add_text", help="add text block")
+    s.add_argument("text")
+    s.add_argument("--size", type=int, default=14)
+    s.add_argument("--x", type=int, default=0)
+    s.add_argument("--y", type=int, default=0)
+    s.set_defaults(func=cmd_add_text)
+
+    # image
+    s = sub.add_parser("add_image", help="add image")
+    s.add_argument("url")
+    s.add_argument("--width", type=int, default=540)
+    s.add_argument("--x", type=int, default=0)
+    s.add_argument("--y", type=int, default=0)
+    s.set_defaults(func=cmd_add_image)
+
+    # template_create
+    s = sub.add_parser("template_create", help="create template from board")
+    s.add_argument("name")
+    s.add_argument("--ids", nargs="*", help="list of item ids")
+    s.set_defaults(func=cmd_template_create)
+
+    # template_export
+    s = sub.add_parser("template_export", help="export template to json")
+    s.add_argument("name")
+    s.set_defaults(func=cmd_template_export)
+
+    # template_import
+    s = sub.add_parser("template_import", help="import template")
+    s.add_argument("name")
+    s.add_argument("--x", type=int, default=0)
+    s.add_argument("--y", type=int, default=0)
+    s.set_defaults(func=cmd_template_import)
 
     return p
 
